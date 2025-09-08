89ed5cc037419e61fc6284aa052d8cb6e7a1f3cc
codingclubnitanp <coding@nitandhra.ac.in>
2025-09-08 20:43:07 +0530

    Update Solution.cpp

--- Day01/Easy/Solution.cpp
+++ Day01/Easy/Solution.cpp
@@ -1,11 +1,25 @@
+#include <iostream>
+using namespace std;
+
 class Solution {
   public:
     int countDigits(int n) {
         int c = 0;
-        while(n > 0){
+        while (n > 0) {
             c++;
-            n = n/10;
+            n = n / 10;
         }
         return c;
     }
 };
+
+int main() {
+    Solution sol;
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Number of digits: " << sol.countDigits(n) << endl;
+    return 0;
+}
