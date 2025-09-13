1abcbda239f755fdde153e4de7360c04674242e0
codingclubnitanp <coding@nitandhra.ac.in>
2025-09-13 17:47:09 +0530

    Create Solution.c

---
+++ Day06/Easy/Solution.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void print_divisors(int n) {
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    print_divisors(n);
+    return 0;
+}
