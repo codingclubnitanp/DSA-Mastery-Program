99349d68826f4c650b61f1687e27cdd2afbe09f7
codingclubnitanp <coding@nitandhra.ac.in>
2025-09-15 18:10:13 +0530

    Update Solution.cpp

--- Day08/Medium/Solution.cpp
+++ Day08/Medium/Solution.cpp
@@ -5,24 +5,31 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
+        vector<pair<int,int>> vs;
+        for(int i=0;i<n;i++){
+            vs.push_back({nums[i],i});
+        }
+        sort(vs.begin(),vs.end());
+        int i = 0, j = n-1;
+        while(i < j){
+            int sum = vs[i].first + vs[j].first;
+            if(sum == target){
+                return {vs[i].second, vs[j].second};
+            } else if(sum < target) {
+                i++;
+            } else {
+                j--;
             }
         }
-        return {}; // problem guarantees one solution
+        return {-1,-1};
     }
 };
 
 int main() {
-    vector<int> nums = {2, 7, 11, 15};
-    int target = 9;
-
     Solution sol;
-    vector<int> result = sol.twoSum(nums, target);
-
-    cout << "Indices: [" << result[0] << ", " << result[1] << "]" << endl;
+    vector<int> nums = {2,7,11,15};
+    int target = 9;
+    vector<int> ans = sol.twoSum(nums, target);
+    cout << ans[0] << " " << ans[1] << endl;
     return 0;
 }
