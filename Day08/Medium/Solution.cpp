bf7f6635745563e53d76903fec4925d20e3f0372
codingclubnitanp <coding@nitandhra.ac.in>
2025-09-15 18:07:29 +0530

    Create Solution.cpp

---
+++ Day08/Medium/Solution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {}; // problem guarantees one solution
+    }
+};
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    Solution sol;
+    vector<int> result = sol.twoSum(nums, target);
+
+    cout << "Indices: [" << result[0] << ", " << result[1] << "]" << endl;
+    return 0;
+}
