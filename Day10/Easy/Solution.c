bc1234d6c959a291588da12e0247f43f0f05820a
codingclubnitanp <coding@nitandhra.ac.in>
2025-09-17 19:16:34 +0530

    Create Solution.c

---
+++ Day10/Easy/Solution.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int fib(int n) {
+    if (n <= 1) return n;
+    int a = 0, b = 1, c = 0;
+    for (int i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+int main() {
+    int n = 10;
+    printf("F(%d) = %d\n", n, fib(n));
+    return 0;
+}
