aac7fca361102587c7e84c8996a53e71d96e8226
codingclubnitanp <coding@nitandhra.ac.in>
2025-09-17 19:15:42 +0530

    Create Solution.cpp

---
+++ Day10/Easy/Solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n) {
+    if (n <= 1) return n;
+    int a = 0, b = 1, c = 0;
+    for (int i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+int main() {
+    int n = 10;
+    cout << "F(" << n << ") = " << fib(n) << endl;
+    return 0;
+}
