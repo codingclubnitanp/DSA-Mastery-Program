9bebdbe97fd1d7b8c2603694d9790438a4c9a316
codingclubnitanp <coding@nitandhra.ac.in>
2025-09-19 00:02:44 +0530

    Create Solution.java

---
+++ Day11/Medium/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        Solution sol = new Solution();
+        int result = sol.maxSubArray(nums);
+        System.out.println("Maximum subarray sum: " + result);
+    }
+}
